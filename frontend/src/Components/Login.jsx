382f3f140e82f45d81e9d7236947d3a4b4723125
kanyijohn <mrkanyi8@gmail.com>
2024-11-09 12:08:48 +0300

    login backend

--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -2,21 +2,37 @@ import React from 'react'
 import './style.css'
 
 const Login = () => {
+
+
+    const [values, setValues] = useState({
+        email: '',
+        password: ''
+    })
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        axios.post('http://localhost:3000/auth/adminlogin')
+        .then(result => console.log (result))
+        .catch(err => console.log (err))
+
+    }
+
+
   return (
     <div className='d-flex justify-content-center align-items-center vh-100 loginPage'>
         <div className='p-3 rounded w-25 border loginForm'>
 
             <h2>Welcome Back</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="email"><strong>Email:</strong></label>
                     <input type="email" name='email' autoComplete='off' placeholder='Enter Email' 
-                    className='form-control rounded-0'/>
+                    onChange={(e) => setValues({...values, email : e.target.value})} className='form-control rounded-0'/>
                 </div>
                 <div>
                     <label htmlFor="password"><strong>Password:</strong></label>
                     <input type="password" name='password' autoComplete='off' placeholder='Enter Password' 
-                    className='form-control rounded-0'/>
+                    onChange={(e) => setValues({...values, password : e.target.value})} className='form-control rounded-0'/>
                 </div>
                 <button className='btn btn-success w-100 rounded-0 mb-2'>Log in</button>
                 <div className='mb-1'> 
