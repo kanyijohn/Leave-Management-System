d629ad5f215f13ff25184bad8b2a7472d568dc63
kanyijohn <mrkanyi8@gmail.com>
2025-02-07 12:24:37 +0300

    AddEmployee

---
+++ frontend/src/Components/AddEmployee.jsx
@@ -0,0 +1,135 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const AddEmployee = () => {
+  const [employee, setEmployee] = useState({
+    name: "",
+    email: "",
+    password: "",
+    phone: "",
+    department_id: "",
+    
+  });
+  const [department, setDepartment] = useState([]);
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/auth/department")
+      .then((result) => {
+        if (result.data.Status) {
+          setDepartment(result.data.Result);
+        } else {
+          alert(result.data.Error);
+        }
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const formData = new FormData();
+    formData.append('name', employee.name);
+    formData.append('email', employee.email);
+    formData.append('password', employee.password);
+    formData.append('phone', employee.phone);
+    formData.append('department_id', employee.department_id);
+
+    axios.post('http://localhost:3000/auth/add_employee', formData)
+    .then(result => {
+        if(result.data.Status) {
+            navigate('/dashboard/employee')
+        } else {
+            alert(result.data.Error)
+        }
+    })
+    .catch(err => console.log(err))
+  }
+
+  return (
+    <div className="d-flex justify-content-center align-items-center mt-3">
+      <div className="p-3 rounded w-50 border">
+        <h3 className="text-center">Add Employee</h3>
+        <form className="row g-1" onSubmit={handleSubmit}>
+          <div className="col-12">
+            <label for="inputName" className="form-label">
+              Name
+            </label>
+            <input
+              type="text"
+              className="form-control rounded-0"
+              id="inputName"
+              placeholder="Enter Name"
+              onChange={(e) =>
+                setEmployee({ ...employee, name: e.target.value })
+              }
+            />
+          </div>
+          <div className="col-12">
+            <label for="inputEmail4" className="form-label">
+              Email
+            </label>
+            <input
+              type="email"
+              className="form-control rounded-0"
+              id="inputEmail4"
+              placeholder="Enter Email"
+              autoComplete="off"
+              onChange={(e) =>
+                setEmployee({ ...employee, email: e.target.value })
+              }
+            />
+          </div>
+          <div className="col-12">
+            <label for="inputPassword4" className="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-control rounded-0"
+              id="inputPassword4"
+              placeholder="Enter Password"
+              onChange={(e) =>
+                setEmployee({ ...employee, password: e.target.value })
+              }
+            />
+            <label for="inputPhone" className="form-label">
+              Phone
+            </label>
+            <input
+              type="text"
+              className="form-control rounded-0"
+              id="inputSalary"
+              placeholder="Enter Phone Number"
+              autoComplete="off"
+              onChange={(e) =>
+                setEmployee({ ...employee, phone: e.target.value })
+              }
+            />
+          </div>
+          
+          <div className="col-12">
+            <label for="category" className="form-label">
+              Department
+            </label>
+            <select name="department" id="department" className="form-select"
+                onChange={(e) => setEmployee({...employee, department_id: e.target.value})}>
+              {department.map((c) => {
+                return <option value={c.id}>{c.department}</option>;
+              })}
+            </select>
+          </div>
+          
+          <div className="col-12">
+            <button type="submit" className="btn btn-primary w-100">
+              Add Employee
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default AddEmployee;
