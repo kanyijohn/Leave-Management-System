bf98d586857ad98c30013cfe6bf5af6a7c80361b
kanyijohn <mrkanyi8@gmail.com>
2025-03-17 11:02:30 +0300

    LeaveManagement

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,46 +22,46 @@ import ApplyLeave from './Components/ApplyLeave'
 import LeaveRequests from './Components/LeaveRequests'
 import EmployeeHome from './Components/EmployeeHome'
 import EditLeaveRequest from './Components/EditLeaveRequest'
+import LeaveManagement from './Components/LeaveManagement'
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Start />}></Route>
-      <Route path='/adminlogin' element={<Login />}></Route>
-      <Route path='/employee_login' element={<EmployeeLogin />}></Route>
+      <Routes>
+        <Route path='/' element={<Start />} />
+        <Route path='/adminlogin' element={<Login />} />
+        <Route path='/employee_login' element={<EmployeeLogin />} />
 
-      <Route path='/employee_dashboard' element={
-        <PrivateRoute >
-          <EmployeeDashboard />
-        </PrivateRoute>
-      }>
+        <Route path="/employee_dashboard" element={
+          <PrivateRoute>
+            <EmployeeDashboard />
+          </PrivateRoute>
+        }>
+          <Route index element={<EmployeeHome />} />
+          <Route path="employeedetail" element={<EmployeeDetail />} />
+          <Route path="applyleave" element={<ApplyLeave />} />
+          <Route path="leaverequests" element={<LeaveRequests />} />
+          <Route path="editleaverequest/:id" element={<EditLeaveRequest />} />
+        </Route>
 
-      <Route path='' element={<EmployeeHome />}></Route>
-      <Route path='/employee_dashboard/employeedetail' element={<EmployeeDetail />}></Route>
-      <Route path='/employee_dashboard/applyleave' element={<ApplyLeave />}></Route>
-      <Route path='/employee_dashboard/leaverequests' element={<LeaveRequests />}></Route>
-      <Route path='/employee_dashboard/editleaverequest/:id' element={<EditLeaveRequest />}></Route>
-      </Route> 
-
-      <Route path='/dashboard' element={
-        <PrivateRoute >
-          <Dashboard />
-        </PrivateRoute>
-      }>
-
-        <Route path='' element={<Home />}></Route>
-        <Route path='/dashboard/employee' element={<Employee />}></Route>
-        <Route path='/dashboard/department' element={<Department />}></Route>
-        <Route path='/dashboard/profile' element={<Profile />}></Route>
-        <Route path='/dashboard/add_department' element={<AddDepartment />}></Route>
-        <Route path='/dashboard/add_employee' element={<AddEmployee />}></Route>
-        <Route path='/dashboard/edit_employee/:id' element={<EditEmployee />}></Route>
-        <Route path='/dashboard/leavetype' element={< LeaveType />}></Route>
-        <Route path='/dashboard/add_leavetype' element={<AddLeaveType />}></Route>
-        <Route path='/dashboard/edit_leavetype/:id' element={<EditLeaveType />}></Route>
-      </Route>
-    </Routes>
+        <Route path='/dashboard' element={
+          <PrivateRoute>
+            <Dashboard />
+          </PrivateRoute>
+        }>
+          <Route index element={<Home />} />
+          <Route path="employee" element={<Employee />} />
+          <Route path="department" element={<Department />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="add_department" element={<AddDepartment />} />
+          <Route path="add_employee" element={<AddEmployee />} />
+          <Route path="edit_employee/:id" element={<EditEmployee />} />
+          <Route path="leavetype" element={<LeaveType />} />
+          <Route path="add_leavetype" element={<AddLeaveType />} />
+          <Route path="edit_leavetype/:id" element={<EditLeaveType />} />
+          <Route path="leavemanagement" element={<LeaveManagement />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   )
 }
